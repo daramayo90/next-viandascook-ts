26db7168859699f61e16e53969baa6c5151fb54b
daramayo90 <da.aramayo1990@gmail.com>
2022-09-22 16:55:58 -0500

    styling footer component - media queries implemented

--- components/ui/Footer.tsx
+++ components/ui/Footer.tsx
@@ -8,42 +8,39 @@ export const Footer = () => {
 
    return (
       <section className={styles.footer}>
-         <div className={styles.footerBack}>
-            <img src='/img/footer-mobile.jpg' alt='Viandas Cook Footer' />
+         <div className={styles.info}>
+            <div className={styles.footerBrand}>
+               <h2 className={styles.title}>Comida Saludable.</h2>
+               <h2 className={styles.title}>Comida Práctica y Casera.</h2>
+               <span className={styles.slogan}>¡Riquísima!</span>
+            </div>
+
+            <div className={styles.columnList}>
+               <ul className={styles.list}>
+                  <li>¿Cómo funciona?</li>
+                  <li>Nosotros</li>
+               </ul>
+               <ul className={styles.list}>
+                  <li>¿Preguntas?</li>
+                  <li>Sumá puntos</li>
+               </ul>
+            </div>
+
+            <div className={styles.contact}>
+               <div className={styles.support}>
+                  <img className={styles.logo} src='/logo/viandascook-logo.png' alt='logo' />
+                  <span className={styles.mail}>info@viandascook.com</span>
+               </div>
+
+               <div className={styles.socialMedia}>
+                  <AiOutlineInstagram className={styles.icon} />
+                  <BsFacebook className={styles.icon} />
+                  <AiOutlineWhatsApp className={styles.icon} />
+               </div>
+            </div>
          </div>
 
-         <div className={styles.footerBrand}>
-            <h2 className={styles.title}>Comida Saludable.</h2>
-            <h2 className={styles.title}>Comida Práctica y Casera.</h2>
-            <span className={styles.slogan}>¡Riquísima!</span>
-         </div>
-
-         <div className={styles.columnList}>
-            <ul className={styles.list}>
-               <li>¿Cómo funciona?</li>
-               <li>Nosotros</li>
-            </ul>
-            <ul className={styles.list}>
-               <li>¿Preguntas?</li>
-               <li>Sumá puntos</li>
-            </ul>
-         </div>
-
-         <div className={styles.mail}>
-            <span>info@viandascook.com</span>
-         </div>
-
-         <div className={styles.logo}>
-            <img src='/logo/viandascook-logo.png' alt='viandascook-logo' />
-         </div>
-
-         <div className={styles.socialMedia}>
-            <AiOutlineInstagram className={styles.icon} />
-            <BsFacebook className={styles.icon} />
-            <AiOutlineWhatsApp className={styles.icon} />
-         </div>
-
-         <div className={styles.copyright}>
+         <div className={styles.legal}>
             <span>Términos y Condiciones</span>
             <span>© Copyright {year} Viandas Cook</span>
          </div>
