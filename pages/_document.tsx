48c27cef10c95c63d168c2d3c6b5359a53f3a4a5
daramayo90 <da.aramayo1990@gmail.com>
2022-09-23 21:23:20 -0500

    remove import fonts url from components

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,9 +11,9 @@ class MyDocument extends Document {
       return (
          <Html>
             <Head>
+               <link href='https://fonts.googleapis.com' rel='preconnect' />
                <link href='http://fonts.cdnfonts.com/css/gilroy-bold' rel='stylesheet' />
                <link href='http://fonts.cdnfonts.com/css/reinata' rel='stylesheet' />
-               <link rel='preconnect' href='https://fonts.googleapis.com' />
                <link
                   href='https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,300;0,400;1,300&family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,300;1,400;1,500;1,700;1,900&display=swap'
                   rel='stylesheet'
