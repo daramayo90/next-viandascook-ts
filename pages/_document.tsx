bed11a8b2abf77689e97875203398a5390228ed6
daramayo90 <da.aramayo1990@gmail.com>
2022-09-23 21:35:44 -0500

    _document updated for new fonts - 2

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,9 +12,9 @@ class MyDocument extends Document {
          <Html>
             <Head>
                <link href='https://fonts.googleapis.com' rel='preconnect' />
-               <link href='http://fonts.cdnfonts.com' rel='preconnect' />
-               <link href='http://fonts.cdnfonts.com/css/gilroy-bold' rel='stylesheet' />
-               <link href='http://fonts.cdnfonts.com/css/reinata' rel='stylesheet' />
+               <link href='https://fonts.cdnfonts.com' rel='preconnect' />
+               <link href='https://fonts.cdnfonts.com/css/gilroy-bold' rel='stylesheet' />
+               <link href='https://fonts.cdnfonts.com/css/reinata' rel='stylesheet' />
                <link
                   href='https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,300;0,400;1,300&family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,300;1,400;1,500;1,700;1,900&display=swap'
                   rel='stylesheet'
