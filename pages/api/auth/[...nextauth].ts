43363cc0c16e2c5dd5ebf52c575723aafc085363
daramayo90 <da.aramayo1990@gmail.com>
2022-09-29 10:51:29 -0500

    fixing nextAuth providers on vercel

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -19,10 +19,10 @@ export const authOptions: NextAuthOptions = {
          },
       }),
       // Github Login
-      GithubProvider({
-         clientId: process.env.GITHUB_ID!,
-         clientSecret: process.env.GITHUB_SECRET!,
-      }),
+      // GithubProvider({
+      //    clientId: process.env.GITHUB_ID!,
+      //    clientSecret: process.env.GITHUB_SECRET!,
+      // }),
       // Google Login
       // GoogleProvider({
       //    clientId: process.env.GOOGLE_ID!,
@@ -45,35 +45,35 @@ export const authOptions: NextAuthOptions = {
       newUser: '/auth/register',
    },
 
-   session: {
-      maxAge: 2592000, // 30 days
-      strategy: 'jwt',
-      updateAge: 86400, // every day
-   },
+   // session: {
+   //    maxAge: 2592000, // 30 days
+   //    strategy: 'jwt',
+   //    updateAge: 86400, // every day
+   // },
 
    // Callbacks
-   callbacks: {
-      async jwt({ token, account, user }) {
-         if (account) {
-            token.accessToken = account.access_token;
-            switch (account.type) {
-               case 'oauth':
-                  token.user = await dbUsers.oAuthToDbUser(user?.email || '', user?.name || '');
-                  break;
+   // callbacks: {
+   //    async jwt({ token, account, user }) {
+   //       if (account) {
+   //          token.accessToken = account.access_token;
+   //          switch (account.type) {
+   //             case 'oauth':
+   //                token.user = await dbUsers.oAuthToDbUser(user?.email || '', user?.name || '');
+   //                break;
 
-               case 'credentials':
-                  token.user = user;
-                  break;
-            }
-         }
-         return token;
-      },
-      async session({ session, token, user }) {
-         session.accessToken = token.access_token;
-         session.user = token.user as any;
-         return session;
-      },
-   },
+   //             case 'credentials':
+   //                token.user = user;
+   //                break;
+   //          }
+   //       }
+   //       return token;
+   //    },
+   //    async session({ session, token, user }) {
+   //       session.accessToken = token.access_token;
+   //       session.user = token.user as any;
+   //       return session;
+   //    },
+   // },
 };
 
 export default NextAuth(authOptions);
