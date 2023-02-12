9d4c15bd9311958588ec0de3768371514329a4fe
daramayo90 <da.aramayo1990@gmail.com>
2023-02-12 13:03:47 -0300

    add new msg for no orders made in 'Mis Pedidos'

--- pages/pedidos/historial.tsx
+++ pages/pedidos/historial.tsx
@@ -10,9 +10,10 @@ import { OrderLayout } from '../../components/layouts';
 import { OrderCard } from '../../components/orders';
 
 import styles from '../../styles/OrdersHistory.module.css';
+import { Button } from '../../components/ui';
 
 interface Props {
-   orders: IOrder[];
+   orders?: IOrder[];
 }
 
 const OrderHistoryPage: NextPage<Props> = ({ orders }) => {
@@ -20,9 +21,22 @@ const OrderHistoryPage: NextPage<Props> = ({ orders }) => {
       <OrderLayout title={'Historial de Pedidos'}>
          <section className={styles.history}>
             <div className={styles.container}>
-               {orders.map((order) => (
-                  <OrderCard key={order._id} order={order} />
-               ))}
+               {orders?.length === 0 ? (
+                  <div className={styles.noOrders}>
+                     <h2 className={styles.title}>Todavía no realizaste ninguna compra</h2>
+                     <span>Realizá tu primer pedido a continuación:</span>
+                     <div className={styles.btn}>
+                        <Button
+                           href={'/menu'}
+                           content={'Ver platos'}
+                           background='var(--primary)'
+                           border='none'
+                        />
+                     </div>
+                  </div>
+               ) : (
+                  orders!.map((order) => <OrderCard key={order._id} order={order} />)
+               )}
             </div>
          </section>
       </OrderLayout>
