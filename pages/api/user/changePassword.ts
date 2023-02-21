15f1723103ba7054d5b9695639bf2a0abfc1e7d6
daramayo90 <da.aramayo1990@gmail.com>
2023-02-20 21:09:29 -0300

    creation of change password page

---
+++ pages/api/user/changePassword.ts
@@ -0,0 +1,67 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { getSession } from 'next-auth/react';
+import { User } from '../../../models';
+import { db } from '../../../database';
+import bcrypt from 'bcryptjs';
+
+type Data = { message: string };
+
+interface ChangePasswordRequestBody {
+   oldPassword: string;
+   newPassword: string;
+}
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+   switch (req.method) {
+      case 'PATCH':
+         return changePassword(req, res);
+
+      default:
+         res.status(400).json({ message: 'Bad request' });
+   }
+}
+
+const changePassword = async (req: NextApiRequest, res: NextApiResponse) => {
+   const session = (await getSession({ req })) as any;
+
+   if (!session) {
+      return res.status(401).json({ error: 'Unauthorized' });
+   }
+
+   const { oldPassword, newPassword }: ChangePasswordRequestBody = req.body;
+
+   await db.connect();
+
+   // Verify the user's old password
+   const user = await User.findById(session.user._id);
+
+   await db.disconnect();
+
+   if (!user) {
+      return res.status(400).json({ message: 'Cliente no encontrado en nuestra base de datos' });
+   }
+
+   const passwordMatch = bcrypt.compareSync(oldPassword, user.password!);
+
+   if (!passwordMatch) {
+      return res.status(400).json({ message: 'La contraseña actual es incorrecta' });
+   }
+
+   // Hash the new password
+   const hashedPassword = bcrypt.hashSync(newPassword, 10);
+
+   await db.connect();
+
+   await User.updateOne(
+      { email: user!.email },
+      {
+         $set: {
+            password: hashedPassword,
+         },
+      },
+   );
+
+   await db.disconnect();
+
+   res.status(200).json({ message: 'Contraseña cambiada con éxito. Redirigiendo..' });
+};
--- pages/mi-cuenta/password.tsx
+++ pages/mi-cuenta/password.tsx
@@ -1,10 +1,119 @@
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+import { useSession } from 'next-auth/react';
+
+import { useForm } from 'react-hook-form';
+
+import { viandasApi } from '../../axiosApi';
 import { ShopLayout } from '../../components/layouts';
+import { SubmitButton } from '../../components/ui';
+
+import styles from '../../styles/Newsletter.module.css';
+
+type FormData = {
+   oldPassword: string;
+   newPassword: string;
+};
 
 const PasswordPage = () => {
+   const session = useSession();
+   const [isClicked, setIsClicked] = useState(false);
+   const [errorMessage, setErrorMessage] = useState('');
+   const [okMessage, setOkMessage] = useState('');
+
+   const router = useRouter();
+
+   const {
+      register,
+      handleSubmit,
+      reset,
+      formState: { errors },
+   } = useForm<FormData>();
+
+   const onChangePassword = async ({ oldPassword, newPassword }: FormData) => {
+      setIsClicked(true);
+
+      try {
+         const { data } = await viandasApi.patch('/user/changePassword', {
+            oldPassword,
+            newPassword,
+         });
+
+         const { message } = data;
+
+         reset();
+         setErrorMessage('');
+         setIsClicked(false);
+         setOkMessage(message);
+         setTimeout(() => {
+            setOkMessage('');
+            router.back();
+         }, 2500);
+      } catch (error: any) {
+         reset();
+         setIsClicked(false);
+         setErrorMessage(error.response.data.message);
+         setTimeout(() => {
+            setErrorMessage('');
+         }, 3000);
+      }
+   };
+
+   if (!session) {
+      return <></>;
+   }
+
    return (
       <ShopLayout title={'Viandas Cook - Cambiar Clave'} pageDescription={''}>
          <section>
-            <div>Cambiar Clave</div>
+            <h1>Change Password</h1>
+            <form onSubmit={handleSubmit(onChangePassword)}>
+               <label className={styles.inputText}>
+                  <span>Contraseña Actual:</span>
+                  <input
+                     {...register('oldPassword', {
+                        required: 'La contraseña es un campo requerido',
+                        minLength: {
+                           value: 6,
+                           message: 'Debe tener al menos 6 caracteres',
+                        },
+                     })}
+                     type='password'
+                  />
+                  {errors.oldPassword && (
+                     <span className={styles.error}>{errors.oldPassword?.message}</span>
+                  )}
+               </label>
+
+               <label className={styles.inputText}>
+                  <span>Contraseña Nueva:</span>
+                  <input
+                     {...register('newPassword', {
+                        required: 'La contraseña es un campo requerido',
+                        minLength: {
+                           value: 6,
+                           message: 'Debe tener al menos 6 caracteres',
+                        },
+                     })}
+                     type='password'
+                  />
+                  {errors.newPassword && (
+                     <span className={styles.error}>{errors.newPassword?.message}</span>
+                  )}
+               </label>
+
+               <div className={errorMessage ? `${styles.errorMessage} fadeIn` : 'noDisplay'}>
+                  <span>{errorMessage}</span>
+               </div>
+
+               <div className={okMessage ? `${styles.okMessage} fadeIn` : 'noDisplay'}>
+                  <span>{okMessage}</span>
+               </div>
+
+               <div className={styles.btn}>
+                  <SubmitButton content='Suscribirse' isClicked={isClicked} />
+               </div>
+            </form>
          </section>
       </ShopLayout>
    );
