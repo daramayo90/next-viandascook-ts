e74affc380dbd16b92a55c7e5b45bae04d2105d2
daramayo90 <da.aramayo1990@gmail.com>
2023-03-22 10:46:28 -0300

    upgrade /nosotros page to improve copy in marketing/seo

--- pages/nosotros/index.tsx
+++ pages/nosotros/index.tsx
@@ -11,26 +11,26 @@ const NosotrosPage = () => {
             <div className={styles.intro}>
                <div className={styles.container}>
                   <div className={styles.videoIntro}>
-                     <video autoPlay muted loop playsInline poster='/nosotros/img-intro.jpg'>
-                        <source src='/nosotros/video-intro.mp4' type='video/mp4' />
+                     <video autoPlay muted loop playsInline poster='/img/nosotros-envasado.jpg'>
+                        <source src='/video/nosotros-intro.mp4' type='video/mp4' />
                      </video>
                   </div>
 
                   <div className={styles.textIntro}>
                      <h1>
-                        Viandas Cook:<br></br> Tu opción más saludable y deliciosa para comer en
-                        casa
+                        Viandas Cook<br></br> Tu opción <strong>saludable y deliciosa</strong>, en
+                        sólo 15 minutos en tu mesa.
                      </h1>
                      <p>
-                        Nuestro equipo de chefs y nutricionistas trabajan juntos para ofrecer una
-                        variedad de menús que se adaptan a tus necesidades y gustos, para que puedas
-                        disfrutar de comidas sabrosas sin sacrificar la calidad ni la salud. ¿Te
-                        animas a probar nuestras viandas?
+                        Ofrecemos una <strong>variedad</strong> de menús que se adaptan a tus
+                        necesidades y gustos, para que puedas disfrutar de comidas sabrosas sin
+                        sacrificar la calidad y el tiempo.
                      </p>
+                     <p>¡Hacé que tu día tenga sabor!</p>
                      <div className={styles.btn}>
                         <Button
                            href={'/menu'}
-                           content={'Ver Menú'}
+                           content={'¡Comprar!'}
                            color='var(--white)'
                            border='2px solid var(--white)'
                         />
@@ -42,12 +42,12 @@ const NosotrosPage = () => {
             <div className={styles.presentation}>
                <h2>Nosotros</h2>
                <p>
-                  Viandas Cook nació de la pasión por la comida saludable y la necesidad de
-                  simplificar la vida de las personas ocupadas. Ofrecemos comidas saludables y
-                  convenientes, sin sacrificar el sabor o la calidad. Trabajamos con los mejores
-                  ingredientes para crear deliciosas comidas que satisfagan el apetito y nutran el
-                  cuerpo. Para nosotros, la comida es más que solo combustible: es una forma de vida
-                  saludable y feliz.
+                  Viandas Cook nació de la pasión por la comida <strong>saludable</strong> y la
+                  necesidad de <strong>simplificar</strong> la vida de las personas. Creamos platos
+                  que te ayudarán a organizarte en medio de tu rutina acelerada, y a llevar una
+                  alimentación sabrosa y balanceada. <strong>¡En sólo 15 minutos!</strong> Para
+                  nosotros, la comida es más que solo combustible: es una forma de vida saludable y
+                  feliz.
                </p>
             </div>
 
@@ -56,7 +56,7 @@ const NosotrosPage = () => {
             <div className={styles.btn}>
                <Button
                   href={'/menu'}
-                  content={'¡Comprar!'}
+                  content={'Ver Platos'}
                   color='var(--black)'
                   border='2px solid var(--black)'
                />
