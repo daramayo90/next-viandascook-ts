2ad21a9cd6da93533eb2a34f76c7c68adfe39bba
daramayo90 <da.aramayo1990@gmail.com>
2023-03-27 18:48:59 -0300

    improve howItWorks page

--- pages/como-funciona/index.tsx
+++ pages/como-funciona/index.tsx
@@ -11,17 +11,18 @@ const ComoFuncionaPage = () => {
             <div className={styles.intro}>
                <div className={styles.container}>
                   <div className={styles.videoIntro}>
-                     <video autoPlay muted loop playsInline poster='/howItWorks/img-intro.jpg'>
-                        <source src='/howItWorks/video-intro.mp4' type='video/mp4' />
+                     <video autoPlay muted loop playsInline poster='/img/como-funciona-intro.jpg'>
+                        <source src='/video/como-funciona-intro.mp4' type='video/mp4' />
                      </video>
                   </div>
 
                   <div className={styles.textIntro}>
-                     <h1>Platos saludables listos para comer elaborados por expertos</h1>
+                     <h1>Platos saludables listos para comer elaborados por expertos.</h1>
                      <p>
                         Nuestros cocineros te llevan a tu mesa una variedad de comidas frescas y
                         saludables todos los días
                      </p>
+
                      <div className={styles.btn}>
                         <Button
                            href={'/menu'}
@@ -35,9 +36,9 @@ const ComoFuncionaPage = () => {
             </div>
 
             <div className={styles.presentation}>
-               <h2>¿Cómo Funciona?</h2>
+               <h2 className={styles.subTitle}>¿Cómo Funciona?</h2>
                <p>
-                  En Viandas Cook te ofrecemos comidas prácticas, rápidas, caseras, ricas y
+                  En <strong>Viandas Cook</strong> te ofrecemos comidas prácticas, caseras, ricas y
                   saludables para hacer tu vida más fácil.
                </p>
             </div>
