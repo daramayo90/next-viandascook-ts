3a697897f63bc9b6e0aacb548a57048d5b4aedea
daramayo90 <da.aramayo1990@gmail.com>
2023-04-19 11:02:47 -0300

    implement GTM

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,6 +14,17 @@ class MyDocument extends Document {
       return (
          <Html>
             <Head>
+               {/* Start Google Tag Manager */}
+               <Script id='google-tag-manager'>
+                  {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                  'https://www.googletagmanager.com/gtm.js?id=%27+i+dl;f.parentNode.insertBefore(j,f);
+                  })(window,document,'script','dataLayer','GTM-WWP3752')
+               `}
+               </Script>
+               {/* End Google Tag Manager */}
+
                {/* Start Google Analytics 4 */}
                <Script
                   async={true}
