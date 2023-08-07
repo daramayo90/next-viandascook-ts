c67dd4620e0223e624748ba038a3c48a00013986
daramayo90 <da.aramayo1990@gmail.com>
2023-08-07 12:48:45 -0300

    update server-sitemap

--- pages/server-sitemap.xml.tsx
+++ pages/server-sitemap.xml.tsx
@@ -1,6 +1,7 @@
 import { GetServerSideProps } from 'next';
 import { ISitemapField, getServerSideSitemapLegacy } from 'next-sitemap';
 import { dbProducts } from '../database';
+import { IProduct } from '../interfaces';
 
 const SiteMap = () => {
    return null;
@@ -9,7 +10,16 @@ const SiteMap = () => {
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
    const products = await dbProducts.getAllProducts();
 
-   const fields: ISitemapField[] = products.map((product) => ({
+   const fields: ISitemapField[] = products.map((product: IProduct) => ({
+      id: product._id,
+      title: product.name,
+      description: product.name,
+      availability: product.inStock ? 'in stock' : 'out of stock',
+      condition: 'new',
+      price: product.price,
+      link: `https://www.viandascook.com/plato/${product.slug}`,
+      image_link: product.image,
+      brand: 'Viandas Cook',
       loc: `https://www.viandascook.com/plato/${product.slug}`,
       lastmod: new Date(product.updatedAt!).toISOString(),
    }));
