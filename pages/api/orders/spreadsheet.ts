3160ab62726a2bf4bd28ab796929db496270886e
daramayo90 <da.aramayo1990@gmail.com>
2023-08-15 17:37:50 -0300

    test spreadsheets api

--- pages/api/orders/spreadsheet.ts
+++ pages/api/orders/spreadsheet.ts
@@ -16,14 +16,17 @@ type SheetForm = {
 };
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+   console.log('1');
    if (req.method !== 'POST') {
       return res.status(405).send({ message: 'Only POST is allowed' });
    }
 
    const { _id, today, email, dni, paymentMethod, total } = req.body as SheetForm;
+   console.log('2');
 
    try {
-      console.log('1');
+      console.log('3');
+
       const auth = new google.auth.GoogleAuth({
          credentials: {
             client_email: process.env.GOOGLE_CLIENT_EMAIL!,
@@ -35,11 +38,14 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             'https://www.googleapis.com/auth/spreadsheets',
          ],
       });
+      console.log('4');
 
       const sheets = google.sheets({
          auth,
          version: 'v4',
       });
+      console.log('5');
+      console.log(sheets.spreadsheets.values);
 
       const response = await sheets.spreadsheets.values.append({
          spreadsheetId: process.env.GOOGLE_SPREADSHEET_ID,
@@ -49,10 +55,13 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             values: [[_id, today, email, dni, paymentMethod, total]],
          },
       });
+      console.log('6');
+      console.log(response);
 
       return res.status(200).json({ data: response.data });
    } catch (e) {
-      // console.log(e);
+      console.log('ERROR');
+      console.log(e);
       return res.status(500).send({ message: 'Algo salió mal' });
    }
 }
