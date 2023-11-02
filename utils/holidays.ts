a9a9d02756593432eb9b38cef759509671120b04
daramayo90 <da.aramayo1990@gmail.com>
2023-11-02 10:48:34 -0300

    update holidays

--- utils/holidays.ts
+++ utils/holidays.ts
@@ -27,6 +27,7 @@ export const holidays = [
    new Date(2023, 9, 13),
    new Date(2023, 9, 16),
    new Date(2023, 9, 30),
+   new Date(2023, 10, 6),
    new Date(2023, 10, 20),
    new Date(2023, 11, 8),
 ];
