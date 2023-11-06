bc0dbfaa45a2824328cb69a322a778023d3a2724
daramayo90 <da.aramayo1990@gmail.com>
2023-11-06 15:40:42 -0300

    SEO improvements - 3

--- components/layouts/MainLayout.tsx
+++ components/layouts/MainLayout.tsx
@@ -95,7 +95,7 @@ const addWebsiteJsonLd = () => {
             "@type": "SearchAction",
             "target": "{search_term_string}",
             "query-input": "required name=search_term_string"
-         },
+         }
       }`,
    };
 };
