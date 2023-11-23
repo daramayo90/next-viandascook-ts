ed400d45b0d35623b760a171e28a9017a9a3d535
daramayo90 <da.aramayo1990@gmail.com>
2023-11-23 13:45:56 -0300

    Add links to admin pages in SideAdminMenu component

--- components/ui/SideAdminMenu.tsx
+++ components/ui/SideAdminMenu.tsx
@@ -1,5 +1,6 @@
 import { useContext } from 'react';
 import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 import { AuthContext, UIContext } from '../../context';
 
@@ -8,9 +9,9 @@ import { HiOutlineClipboardList, HiOutlineLogout } from 'react-icons/hi';
 import { MdOutlineKeyboardArrowRight, MdOutlineSpaceDashboard } from 'react-icons/md';
 import { TfiDropbox } from 'react-icons/tfi';
 import { TbChefHat } from 'react-icons/tb';
+import { BsBagCheck } from 'react-icons/bs';
 
 import styles from '../../styles/SideMenu.module.css';
-import { BsBagCheck } from 'react-icons/bs';
 
 export const SideAdminMenu = () => {
    const router = useRouter();
@@ -19,54 +20,60 @@ export const SideAdminMenu = () => {
 
    const { isAdminMenuOpen, toggleAdminMenu } = useContext(UIContext);
 
-   const navigateTo = (url: string) => {
-      toggleAdminMenu();
-      router.push(url);
-   };
-
    return (
       <section
          className={isAdminMenuOpen ? `${styles.sidemenu} ${styles.open}` : `${styles.sidemenu}`}>
-         <div
-            className={isAdminMenuOpen ? `${styles.options} ${styles.open}` : `${styles.options}`}>
+         <div className={isAdminMenuOpen ? `${styles.options} ${styles.open}` : `${styles.options}`}>
             <ul className={styles.list}>
-               <li onClick={() => navigateTo('/admin')}>
-                  <MdOutlineSpaceDashboard className={styles.icon} />
-                  <span>Dashboard</span>
-                  <MdOutlineKeyboardArrowRight className={styles.iconRight} />
-               </li>
+               <Link href='/admin'>
+                  <a className={styles.linkItem} onClick={toggleAdminMenu}>
+                     <MdOutlineSpaceDashboard className={styles.icon} />
+                     <span>Admin</span>
+                     <MdOutlineKeyboardArrowRight className={styles.iconRight} />
+                  </a>
+               </Link>
 
-               <li onClick={() => navigateTo('/admin/orders')}>
-                  <HiOutlineClipboardList className={styles.icon} />
-                  <span>Pedidos</span>
-                  <MdOutlineKeyboardArrowRight className={styles.iconRight} />
-               </li>
+               <Link href='/admin/orders'>
+                  <a className={styles.linkItem} onClick={toggleAdminMenu}>
+                     <HiOutlineClipboardList className={styles.icon} />
+                     <span>Pedidos</span>
+                     <MdOutlineKeyboardArrowRight className={styles.iconRight} />
+                  </a>
+               </Link>
 
-               <li onClick={() => navigateTo('/admin/products')}>
-                  <TfiDropbox className={styles.icon} />
-                  <span>Productos</span>
-                  <MdOutlineKeyboardArrowRight className={styles.iconRight} />
-               </li>
+               <Link href='/admin/products'>
+                  <a className={styles.linkItem} onClick={toggleAdminMenu}>
+                     <TfiDropbox className={styles.icon} />
+                     <span>Productos</span>
+                     <MdOutlineKeyboardArrowRight className={styles.iconRight} />
+                  </a>
+               </Link>
 
-               <li onClick={() => navigateTo('/admin/users')}>
-                  <AiOutlineUser className={styles.icon} />
-                  <span>Usuarios</span>
-                  <MdOutlineKeyboardArrowRight className={styles.iconRight} />
-               </li>
+               <Link href='/admin/users'>
+                  <a className={styles.linkItem} onClick={toggleAdminMenu}>
+                     <AiOutlineUser className={styles.icon} />
+                     <span>Usuarios</span>
+                     <MdOutlineKeyboardArrowRight className={styles.iconRight} />
+                  </a>
+               </Link>
 
-               <li onClick={() => navigateTo('/cocina/preparar-platos')}>
-                  <TbChefHat className={styles.icon} />
-                  <span>Preparar platos</span>
-                  <MdOutlineKeyboardArrowRight className={styles.iconRight} />
-               </li>
+               <Link href='/admin/preparar-platos'>
+                  <a className={styles.linkItem} onClick={toggleAdminMenu}>
+                     <TbChefHat className={styles.icon} />
+                     <span>Preparar platos</span>
+                     <MdOutlineKeyboardArrowRight className={styles.iconRight} />
+                  </a>
+               </Link>
 
-               <li onClick={() => navigateTo('/cocina/preparar-pedidos')}>
-                  <BsBagCheck className={styles.icon} />
-                  <span>Preparar pedidos</span>
-                  <MdOutlineKeyboardArrowRight className={styles.iconRight} />
-               </li>
+               <Link href='/admin/preparar-pedidos'>
+                  <a className={styles.linkItem} onClick={toggleAdminMenu}>
+                     <BsBagCheck className={styles.icon} />
+                     <span>Preparar pedidos</span>
+                     <MdOutlineKeyboardArrowRight className={styles.iconRight} />
+                  </a>
+               </Link>
 
-               <li onClick={logout}>
+               <li onClick={logout} className={styles.linkItem}>
                   <HiOutlineLogout className={styles.icon} />
                   <span>Cerrar Sesión</span>
                   <MdOutlineKeyboardArrowRight className={styles.iconRight} />
