c91c47dab18d9693a4a1bd28fd1adc97a9e6815a
daramayo90 <da.aramayo1990@gmail.com>
2023-11-28 12:00:28 -0300

    Improve _app optimization

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
 
+import { useLoader } from '../hooks';
 import { ga, meta } from '../utils';
+
+import { CartProvider, OrdersProvider, EmailsProvider } from '../context/dynamic';
+import WhatsApp from '../context/dynamic/FloatingWhatsApp';
+
+import LoadingPage from '../components/ui/Loading';
 
 import '../styles/globals.css';
 
@@ -8,7 +15,53 @@ function MyApp({ Component, pageProps }: AppProps) {
    ga.useGoogleAnalytics();
    meta.useMetaPixel();
 
-   return <Component {...pageProps} />;
+   const { loading } = useLoader();
+
+   const router = useRouter();
+
+   const isMainPage = router.pathname === '/';
+
+   return (
+      <>
+         {isMainPage ? (
+            <PageContent />
+         ) : (
+            <CartProvider>
+               <OrdersProvider>
+                  <EmailsProvider>
+                     <PageContent />
+                  </EmailsProvider>
+               </OrdersProvider>
+            </CartProvider>
+         )}
+      </>
+   );
+
+   function PageContent() {
+      return (
+         <>
+            {loading ? (
+               <LoadingPage />
+            ) : (
+               <>
+                  <WhatsApp
+                     phoneNumber='+5491171080193'
+                     accountName='Pame'
+                     allowEsc
+                     allowClickAway
+                     avatar='/logo/wp-logo.jpg'
+                     chatMessage='Hola 👋, te saluda Pame de Viandas Cook.. ¿cómo puedo ayudarte?'
+                     statusMessage='Atención de 09 a 19hs'
+                     placeholder='Escribe tu mensaje..'
+                     darkMode
+                     chatboxStyle={{ bottom: '9rem' }}
+                  />
+                  <Component {...pageProps} />
+               </>
+            )}
+         </>
+      );
+   }
 }
 
 export default MyApp;
