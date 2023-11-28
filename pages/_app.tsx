a755cf6b8ef529078a9e72d84d5a873160b87678
daramayo90 <da.aramayo1990@gmail.com>
2023-11-28 12:07:03 -0300

    Improve _app optimization

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,21 @@
 import type { AppProps } from 'next/app';
 import { useRouter } from 'next/router';
 
-import { useLoader } from '../hooks';
+import { SessionProvider } from 'next-auth/react';
+import { ThemeProvider } from '@mui/material';
+
+import { SWRConfig } from 'swr';
 import { ga, meta } from '../utils';
 
-import { CartProvider, OrdersProvider, EmailsProvider } from '../context/dynamic';
-import WhatsApp from '../context/dynamic/FloatingWhatsApp';
+import {
+   CartProvider,
+   OrdersProvider,
+   EmailsProvider,
+   UIProvider,
+   AuthProvider,
+} from '../context/dynamic';
 
-import LoadingPage from '../components/ui/Loading';
+import { lightTheme } from '../themes';
 
 import '../styles/globals.css';
 
@@ -15,52 +23,56 @@ function MyApp({ Component, pageProps }: AppProps) {
    ga.useGoogleAnalytics();
    meta.useMetaPixel();
 
-   const { loading } = useLoader();
-
    const router = useRouter();
 
    const isMainPage = router.pathname === '/';
 
+   const isAdminRoute =
+      router.pathname.startsWith('/admin') ||
+      router.pathname.startsWith('/cocina') ||
+      router.pathname.startsWith('/onlera');
+
    return (
-      <>
-         {isMainPage ? (
-            <PageContent />
-         ) : (
-            <CartProvider>
-               <OrdersProvider>
-                  <EmailsProvider>
-                     <PageContent />
-                  </EmailsProvider>
-               </OrdersProvider>
-            </CartProvider>
-         )}
-      </>
+      <SessionProvider>
+         <AuthProvider>
+            <UIProvider>
+               {isAdminRoute ? (
+                  <SWRConfig
+                     value={{
+                        fetcher: (resource, init) => fetch(resource, init).then((res) => res.json()),
+                     }}>
+                     <ThemeProvider theme={lightTheme}>
+                        <AppContent />
+                     </ThemeProvider>
+                  </SWRConfig>
+               ) : (
+                  <AppContent />
+               )}
+            </UIProvider>
+         </AuthProvider>
+      </SessionProvider>
    );
 
-   function PageContent() {
+   function AppContent() {
       return (
          <>
-            {loading ? (
-               <LoadingPage />
+            {isMainPage ? (
+               <PageContent />
             ) : (
-               <>
-                  <WhatsApp
-                     phoneNumber='+5491171080193'
-                     accountName='Pame'
-                     allowEsc
-                     allowClickAway
-                     avatar='/logo/wp-logo.jpg'
-                     chatMessage='Hola 👋, te saluda Pame de Viandas Cook.. ¿cómo puedo ayudarte?'
-                     statusMessage='Atención de 09 a 19hs'
-                     placeholder='Escribe tu mensaje..'
-                     darkMode
-                     chatboxStyle={{ bottom: '9rem' }}
-                  />
-                  <Component {...pageProps} />
-               </>
+               <CartProvider>
+                  <OrdersProvider>
+                     <EmailsProvider>
+                        <PageContent />
+                     </EmailsProvider>
+                  </OrdersProvider>
+               </CartProvider>
             )}
          </>
       );
+   }
+
+   function PageContent() {
+      return <Component {...pageProps} />;
    }
 }
 
