46c2ca5261b18b8daa3659acab9a0d9ab4236d35
Damian Aramayo <da.aramayo1990@gmail.com>
2023-12-10 21:47:04 -0300

    Implement christmas news banner

--- pages/menu/index.tsx
+++ pages/menu/index.tsx
@@ -85,7 +85,7 @@ const ProductsPage: NextPage<Props> = ({ products }) => {
          <section className={styles.products}>
             <Breadcrumbs />
 
-            {/* <News /> */}
+            <News />
 
             <SearchProducts searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
 
