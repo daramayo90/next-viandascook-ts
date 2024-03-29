e1a9f0fbfac8f3274d395aa27758a3a6cbafddd4
Damian Aramayo <da.aramayo1990@gmail.com>
2024-03-29 11:39:18 -0300

    Add new holiday

--- utils/holidays.ts
+++ utils/holidays.ts
@@ -58,6 +58,7 @@ export const holidays = [
    new Date(2024, 2, 29),
    new Date(2024, 3, 1),
    new Date(2024, 3, 2),
+   new Date(2024, 3, 3),
    new Date(2024, 4, 1),
    new Date(2024, 5, 17),
    new Date(2024, 5, 20),
