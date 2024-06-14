39a4b5b27ecbcf7327e1a5781ab3d373d79076bc
Damian Aramayo <da.aramayo1990@gmail.com>
2024-06-14 13:50:17 -0300

    Update email validation regex to include additional domain extensions

--- utils/validations.ts
+++ utils/validations.ts
@@ -2,7 +2,7 @@ export const isValidEmail = (email: string): boolean => {
    const match = String(email)
       .toLowerCase()
       .match(
-         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+(com|ar|net|xyz|app|es|edu|edu\.ar|educ\.ar)))$/,
+         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+(com|ar|net|xyz|app|es|acm|acm\.org|edu|edu\.ar|educ\.ar)))$/,
       );
 
    return !!match;
