14d23d31094c016036739cc46b49e4f1b2508846
Damian Aramayo <da.aramayo1990@gmail.com>
2024-08-14 21:57:05 -0300

    Improve discount cards

--- utils/discounts.ts
+++ utils/discounts.ts
@@ -1,20 +1,20 @@
 import { cloudDiscountsPath } from './cloudinary';
 
 export const discounts = [
-   // {
-   //    name: 'Descuento primera compra',
-   //    img: `${cloudDiscountsPath}/kg03vy8hkzpsej2rqex3`,
-   // },
+   {
+      name: 'Descuento primera compra', // 10% off
+      img: `${cloudDiscountsPath}/cy30lfszotlc21waykp6`,
+   },
    {
       name: 'Descuento llevando 14 viandas o más',
-      img: `${cloudDiscountsPath}/yvh9u82mdmzrlzixfxjr`,
+      img: `${cloudDiscountsPath}/yuu3r0jzxbs8pztrdinv`,
    },
    {
-      name: 'Descuento llevando 28 viandas o más',
-      img: `${cloudDiscountsPath}/huwv7w5kvkdz3fk6olgr`,
+      name: 'Descuento llevando 28 viandas o más', // 10% off
+      img: `${cloudDiscountsPath}/v1hn2wclyfcrcsrx1axh`,
    },
    {
-      name: 'Descuento llevando 56 viandas o más',
-      img: `${cloudDiscountsPath}/i5iikxzlvuekcfv90avt`,
+      name: 'Descuento llevando 56 viandas o más', // 15% off
+      img: `${cloudDiscountsPath}/shxm0eh0gijgxkglwhod`,
    },
 ];
