222b00cd8b92b964d876b62a57962209173f4f9a
Damian Aramayo <da.aramayo1990@gmail.com>
2024-08-26 11:14:30 -0300

    New holidays

--- utils/holidays.ts
+++ utils/holidays.ts
@@ -77,6 +77,8 @@ export const holidays = [
    new Date(2024, 7, 22),
    new Date(2024, 7, 23),
    new Date(2024, 7, 26),
+   new Date(2024, 7, 28),
+   new Date(2024, 7, 29),
    new Date(2024, 9, 11),
    new Date(2024, 10, 18),
    new Date(2024, 11, 24),
