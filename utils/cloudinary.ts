782aec658b85ce7d5afedf2de3c89c7acec3c4da
daramayo90 <da.aramayo1990@gmail.com>
2024-11-05 09:15:42 -0300

    Update product prices on cyber monday

--- utils/cloudinary.ts
+++ utils/cloudinary.ts
@@ -1,3 +1,4 @@
+// cloudinary
 const pathImg = 'https://res.cloudinary.com/viandascook/image/upload/f_auto,q_auto/v1/imgs';
 const pathDiscounts = 'https://res.cloudinary.com/viandascook/image/upload/f_auto,q_auto/v1/discounts';
 const pathVideo = 'https://res.cloudinary.com/viandascook/video/upload/f_auto:video,q_auto/v1/videos';
